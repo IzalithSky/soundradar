ec5f756657cf3db75b046b732b85988c10276d6a
Sky <sky@mail.com>
2022-05-01 22:43:08 +0100

    code cleanup

--- soundradar.py
+++ soundradar.py
@@ -1,5 +1,4 @@
 import pyaudio
-import wave
 import numpy as np 
 import time
 import librosa
@@ -10,9 +9,8 @@ import matplotlib as mpl
 FORMAT = pyaudio.paFloat32
 CHANNELS = 2 
 RATE = 48000
-CHUNK = 11024 
-RECORD_SECONDS = .5
-WAVE_OUTPUT_FILENAME = "recordedFile.wav"
+CHUNK = 320000 
+RECORD_SECONDS = 5
 audio = pyaudio.PyAudio()
 
 def getVolRatio(sampleL, sampleR):
@@ -20,21 +18,18 @@ def getVolRatio(sampleL, sampleR):
     volR = np.sum(sampleR ** 2) / len(sampleR)
     return 100 * ((volL - volR) / (volL + volR))
 
+
 def onStreamData(in_data, frame_count, time_info, flag):
     result = np.fromstring(in_data, dtype = np.float32)
     result = np.reshape(result, (frame_count, CHANNELS))
     left = result[:, 0]
     right = result[:, 1]
 
-    volumeRatio = getVolRatio(left, right)
-
     X = librosa.stft(left)
     Xdb = librosa.amplitude_to_db(abs(X))
     plt.figure(figsize = (15, 3))
     ld.specshow(Xdb, sr = RATE, x_axis = 'time', y_axis = 'hz') 
     plt.colorbar()
-
-    #print("%.2f" % volumeRatio, flush=True)
     
     return(None, pyaudio.paContinue)
 
@@ -47,28 +42,26 @@ for i in range(0, numdevices):
 
 print("-------------------------------------------------------------")
 
-#index = int(input())
-#print("recording via index " + str(index))
+index = int(input())
+print("recording via index " + str(index))
 
 stream = audio.open(
         format = FORMAT,
         channels = CHANNELS,
         rate = RATE,
-        #output = True,
         input = True,
-        #input_device_index = index,
+        input_device_index = index,
         frames_per_buffer = CHUNK)
         #stream_callback = onStreamData)
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    onStreamData(data, CHUNK, None, None)
-    plt.show()    
-
 print ("recording started")
 
+data = stream.read(CHUNK)
+onStreamData(data, CHUNK, None, None)
+plt.show()
+
 #stream.start_stream()
-time.sleep(RECORD_SECONDS)
+#time.sleep(RECORD_SECONDS)
 
 print ("recording stopped")
 
